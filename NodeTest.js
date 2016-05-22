47cff7ac5e18199fade18e06fe15616bb1ca634a
dkrist <davidpeterkrist@gmail.com>
2016-05-22 08:39:39 -0400

    Add files via upload

---
+++ NodeTest.js
@@ -0,0 +1,10 @@
+﻿var express = require('express');
+var app = express();
+
+app.get('/MyHomePage', function (req, res) {
+  res.send('Hello World!');
+});
+
+var nodeServer = app.listen(22, function () {
+  console.log('Example app listening on port 22!' + ' Server: ' + nodeServer.address().address);
+});
